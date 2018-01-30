acf985ba0d3a023cc08628bdb98e351a1add71b8
Arcu5 <35383816+Arcu5@users.noreply.github.com>
2018-01-30 13:55:15 -0500

    periodicWorking

--- periodic/sketch.js
+++ periodic/sketch.js
@@ -1,13 +1,22 @@
+var amp=20;    //Amplite of the wave. How much the ellipses vary vertically from original yPos
+var xSpace=15; //Spacing between each ellipse along the horizontal plane
+var unitSize=10; //Unit size of each ellipse(circle)
+var yDistance=200; //Total length of the sine wave in canvas units
+var x=20;        //Frequency Modifier?
 function setup() {
   createCanvas(1000,1000)
+  frameRate(10)
 }
 
 function draw() {
-  clear();
-  for (var i=0; i<480; i+=20) {
-    ellipse(100+i,300+10*sin(frameCount),10,10);
+ clear();
+  
+  for (var i=0; i < yDistance; i+=xSpace) {
+    ellipse(100+i,280+amp*sin(frameCount+i/x),unitSize,unitSize); //yPos+amp*sin(frameCount+b) where b != 0.
+    //This gives variation to each ellipse, simulating a sine wave like function
+  
   }
+}
   
 
  
-}
